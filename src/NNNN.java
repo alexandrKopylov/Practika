062379d604cf24ee13e2b009612132da057febf9
Sasha <SASH-2007@YA.RU>
2025-11-08 00:39:57 +0500

    sprint_4_object_lesson_2_end

--- src/NNNN.java
+++
@@ -1,5 +0,0 @@
-public class NNNN {
-    public static void main(String[] args) {
-        System.out.println("qwwerr");
-    }
-}
--- src/sprint_04/Object/lesson_2/LostAndFoundOffi.java
+++ src/sprint_04/Object/lesson_2/LostAndFoundOffi.java
@@ -2,16 +2,22 @@ package sprint_04.Object.lesson_2;
 
 import java.util.ArrayList;
 
-public class LostAndFoundOffice {
+ class LostAndFoundOffice {
     // создайте список things
+    ArrayList<Object> things = new ArrayList<>();
 
     // реализуйте метод put()
+    public void put(Object object) {
+        things.add(object);
+    }
 
     // реализуйте метод check()
-
-    for (Object object : things) { // логика проверки вещи в списке
-        if(object.equals(target)) {
-            return true;
+    public boolean check(Object target) {
+        for (Object object : things) { // логика проверки вещи в списке
+            if (object.equals(target)) {
+                return true;
+            }
         }
-
+        return false;
     }
+}
