43108d6674aec6afee994402dd0367168b2aedb5
Sasha <SASH-2007@YA.RU>
2025-11-08 00:12:05 +0500

    sprint_4_object_lesson_2_start

---
+++ src/sprint_04/Object/lesson_2/LostAndFoundOffi.java
@@ -0,0 +1,17 @@
+package sprint_04.Object.lesson_2;
+
+import java.util.ArrayList;
+
+public class LostAndFoundOffice {
+    // создайте список things
+
+    // реализуйте метод put()
+
+    // реализуйте метод check()
+
+    for (Object object : things) { // логика проверки вещи в списке
+        if(object.equals(target)) {
+            return true;
+        }
+
+    }
