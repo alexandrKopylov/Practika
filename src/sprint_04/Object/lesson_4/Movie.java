98bae1f4679f0e00d5556155fbda7ba8822eba6c
Sasha <SASH-2007@YA.RU>
2025-11-08 18:04:46 +0500

    sprint_04.Object.lesson_4 end

--- src/sprint_04/Object/lesson_4/Movie.java
+++ src/sprint_04/Object/lesson_4/Movie.java
@@ -1,9 +1,32 @@
 package sprint_04.Object.lesson_4;
+
+import java.util.Objects;
+
 public class Movie {
     String title; // название фильма
     int releaseYear; // год выхода на экраны
 
+    public Movie(String title, int releaseYear) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return releaseYear == movie.releaseYear && Objects.equals(title, movie.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, releaseYear);
+    }
+
     public String description() { // метод для вывода описания фильма
         return '"' + title + "\" (" + releaseYear + " год)";
     }
+
+
 }
