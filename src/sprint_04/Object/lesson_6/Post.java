8a49001e9b0fdf586613b7a9f1c9943479c6fff3
Sasha <SASH-2007@YA.RU>
2025-11-08 23:11:28 +0500

    package sprint_04.Object.lesson_6; start

---
+++ src/sprint_04/Object/lesson_6/Post.java
@@ -0,0 +1,21 @@
+package sprint_04.Object.lesson_6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Post {
+    private String title; // заголовок
+    private String content; // содержание
+    private String[] tags; // теги
+    private ArrayList<PostComment> comments; //комментарии
+
+
+
+    /* Вывод должен получиться таким:
+Post{title='xxx',     content.length='x',    tags=[x,x],
+comments=[PostComment{text='x!', whoLiked=[x, x]},
+PostComment{text='x', whoLiked=[x,x]},
+PostComment{text='x', whoLiked=null}]} */
+
+
+}
---
+++ src/sprint_04/Object/lesson_6/PostComment.java
@@ -0,0 +1,10 @@
+package sprint_04.Object.lesson_6;
+
+import java.util.Arrays;
+
+public class PostComment {
+    private String text; // содержание комментария
+    private String[] whoLiked; // кто поддержал
+
+
+}
