f66e21d52b59aa1b83e486bd5a5d3c78e1065dd1
Sasha <SASH-2007@YA.RU>
2025-11-09 17:13:50 +0500

    sprint_04.static_const_enum.lesson_4;

---
+++ src/sprint_04/static_const_enum/lesson_4/Practicum.java
@@ -0,0 +1,18 @@
+package sprint_04.static_const_enum.lesson_4;
+
+import java.util.Scanner;
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Пожалуйста, введите сумму перевода в рублях.");
+        // считайте сумму перевода при помощи scanner.nextDouble()
+
+        boolean isValid = ... // добавьте вызов метод isValidAmount
+        if (isValid)
+            System.out.println("Спасибо! Ваш перевод на сумму " + amount + " р. успешно выполнен.");
+    }
+}
+}
