083b93f3996dfb66693ece6673c9f250458ea562
Sasha <SASH-2007@YA.RU>
2025-11-16 13:56:13 +0500

    package sprint_05.oop_abstraction_and_polymorphizm.lesson_3_interface.task1;   start

---
+++ src/sprint_05/oop_abstraction_and_polymorphizm/lesson_3_interface/task1/Practicum.java
@@ -0,0 +1,35 @@
+package sprint_05.oop_abstraction_and_polymorphizm.lesson_3_interface.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface NoteBook {
+    void addNote(String note);
+
+    void deleteNote(int index);
+}
+
+class CalendarApp implements NoteBook {
+    List<String> notes = new ArrayList<>();
+
+    @Override
+    public void addNote(String note) {
+        notes.add(note);
+        System.out.println("Заметка успешно добавлена!");
+    }
+
+
+
+}
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        CalendarApp noteBook = new CalendarApp();
+        noteBook.addNote("Зайти в магазин после работы.");
+        noteBook.addNote("Позвонить маме.");
+
+        noteBook.deleteNote(0);
+    }
+
+}
