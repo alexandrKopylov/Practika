9b5169c1f0a4c62a4ea4fbbaf1cf9fc2b4d2c220
Sasha <SASH-2007@YA.RU>
2025-11-18 23:02:17 +0500

    package sprint_05.oop_Generic.lesson_1.task_3; start

---
+++ src/sprint_05/oop_Generic/lesson_1/task_3/Practicum.java
@@ -0,0 +1,42 @@
+package sprint_05.oop_Generic.lesson_1.task_3;
+
+import java.util.*;
+
+class MyUtils {
+    public static <T> void printAll(ArrayList<T> list) {
+        for (T element : list) {
+            System.out.println(element);
+        }
+    }
+
+    public static <T> T getFirstElement(ArrayList<T> list) {
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    // Реализуйте здесь метод countMatches
+    public static ... ... countMatches(... list, ... sample) {
+        int count = 0;
+        for (... element : list) {
+            if (...) {
+                ... ;
+            }
+        }
+        return count;
+    }
+}
+public class Practicum {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("яблоко");
+        fruits.add("груша");
+        fruits.add("яблоко");
+        fruits.add("слива");
+
+        int count = MyUtils.countMatches(fruits, "яблоко");
+
+        System.out.println("Количество яблок: " + count);
+    }
+}
