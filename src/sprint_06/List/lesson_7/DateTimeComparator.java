b0eea8cb5901f6e8ac00d3a5d7fc3e158a636387
Sasha <SASH-2007@YA.RU>
2025-12-10 21:02:03 +0500

    package sprint_06.vvedenie_v_algoritmi.lesson_8

--- src/sprint_06/List/lesson_7/DateTimeComparator.java
+++ src/sprint_06/List/lesson_7/DateTimeComparator.java
@@ -6,8 +6,38 @@ public class DateTimeComparator implements Comparator<DateTime> {
 
     @Override
     public int compare(DateTime dt1, DateTime dt2) {
-        return Integer.compare(dt1.year, dt2.year);
 
 
+        int year = Integer.compare(dt1.year, dt2.year);
+        if (year != 0) {
+            return year;
+        }
+
+        int month = Integer.compare(dt1.month, dt2.month);
+        if (month != 0) {
+            return month;
+        }
+
+        int day = Integer.compare(dt1.day, dt2.day);
+        if (day != 0) {
+            return day;
+        }
+
+        int hours = Integer.compare(dt1.hours, dt2.hours);
+        if (hours != 0) {
+            return hours;
+        }
+
+        int minutes = Integer.compare(dt1.minutes, dt2.minutes);
+        if (minutes != 0) {
+            return minutes;
+        }
+
+        int seconds = Integer.compare(dt1.seconds, dt2.seconds);
+        if (seconds != 0) {
+            return seconds;
+        }
+
+        return 0;
     }
 }
