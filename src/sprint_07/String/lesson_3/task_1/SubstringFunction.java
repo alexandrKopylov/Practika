28354729faaf6789273baf53f124c46dcbc92927
alexx <sash-2007@ya.ru>
2025-12-23 16:05:24 +0500

    package sprint_07.String.lesson_3.task_1;

---
+++ src/sprint_07/String/lesson_3/task_1/SubstringFunction.java
@@ -0,0 +1,7 @@
+package sprint_07.String.lesson_3.task_1;
+
+public class SubstringFunctions {
+    public boolean startsWith(String initial, String other) {
+        ... // реализуйте метод
+    }
+}
