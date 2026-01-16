d8f2690476ffef51b68c2f0c8cd7f9a6c55391ba
alexx <sash-2007@ya.ru>
2026-01-16 17:22:11 +0500

    package sprint_08.s8_1_stream.lesson_3.task2;

---
+++ src/sprint_08/s8_1_stream/lesson_3/task1/Practicum.java
@@ -0,0 +1,67 @@
+package sprint_08.s8_1_stream.lesson_3.task1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Practicum {
+    public static void main(String[] args) {
+        List<Circle> circles = Arrays.asList(
+                new Circle(3, 5, 12),
+                new Circle(1, -2, 4),
+                new Circle(8, 8, 8),
+                new Circle(5, 1, 8)
+        );
+
+        circles.sort((Circle circle1, Circle circle2) -> {
+            //вставьте код лямбды здесь
+            int razn = circle1.getRadius() - circle2.getRadius();
+            if (razn == 0) {
+                return (circle1.getCenterX() + circle1.getCenterY()) - (circle2.getCenterX() + circle2.getCenterY());
+            } else {
+                return razn;
+            }
+
+        });
+
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
+
+
+class Circle {
+    private int centerX;
+    private int centerY;
+    private int radius;
+
+    public Circle(int centerX, int centerY, int radius) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Радиус окружности: " + radius
+                + ", координаты: "
+                + centerX
+                + ", "
+                + centerY;
+    }
+
+    //добавьте геттеры для координат центра
+
+
+    public int getCenterX() {
+        return centerX;
+    }
+
+    public int getCenterY() {
+        return centerY;
+    }
+}
